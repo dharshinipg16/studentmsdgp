3a3109d7d76dd2616536d56ae97d45d513acf2b5
dharshinipg16 <pgdharshini16@gmail.com>
2024-03-06 13:16:29 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -64,6 +64,8 @@ app.delete("/delete/:id", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+const PORT=process.env.PORT||3000;
+
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
